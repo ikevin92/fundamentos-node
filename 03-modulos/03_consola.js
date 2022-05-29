4d88dba5346ad9e4ccaa58849c8c2a8780373556
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-05-29 15:13:56 -0500

    console

---
+++ 03-modulos/03_consola.js
@@ -0,0 +1,16 @@
+console.info('info');
+console.error('error');
+console.warn('warn');
+console.table(['uno', 'dos', 'tres']);
+
+console.group('conver');
+console.log('Hola');
+console.log('Mundo');
+console.groupEnd('conver');
+
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');

bff41c59ddc73f8ec72fac60d32c84be11d3d2b8
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-05-28 21:01:16 -0500

    monohilo

---
+++ conceptos/01_monohilo.js
@@ -0,0 +1,15 @@
+console.log('Hola Mundo');
+let i = 0;
+
+//Event loop
+setInterval(() => {
+   console.log(i);
+   console.log('sigo activo');
+   i++;
+
+   if (i === 5) {
+      var a = 3 + z;
+   }
+}, 1000);
+
+console.log('segunda instruccion');
